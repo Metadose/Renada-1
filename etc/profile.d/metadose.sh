e61fc1d71f92dfe2a05744ea8e361a8e8373175e
VicCebedo <cebedo.vii@gmail.com>
2015-11-19 11:10:09 +0800

    Added new environment variables.

--- etc/profile.d/metadose.sh
+++ etc/profile.d/metadose.sh
@@ -1,3 +1,5 @@
 export RENADA_HOME=/srv/git/Renada-1
 export CMP_LOGS=/var/log/cmp/system/logs
 export TOMCAT_LOGS=/var/log/tomcat
+export TOMCAT_WEBAPPS=/usr/share/tomcat/webapps
+export PRAC_DEV=/srv/git/PracticeRepo-dev/mod-webapp-codebase
