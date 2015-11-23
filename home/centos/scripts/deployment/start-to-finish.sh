caa7f0dbc4630167f99a3931d6fde8d83d222dec
VicCebedo <cebedo.vii@gmail.com>
2015-11-23 17:37:57 +0800

    Updated deployment script.

--- home/centos/scripts/deployment/start-to-finish.sh
+++ home/centos/scripts/deployment/start-to-finish.sh
@@ -4,4 +4,4 @@ sh ~/scripts/deployment/start.sh
 
 sudo cp ~/system.properties $TOMCAT_WEBAPPS/pmsys/WEB-INF/classes/system.properties
 
-sh ~/scripts/deployment-finish.sh
+sh ~/scripts/deployment/finish.sh
