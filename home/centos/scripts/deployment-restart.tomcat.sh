0c7c1ed5fb4a95000d082d7eae17d17697a02354
VicCebedo <cebedo.vii@gmail.com>
2015-11-23 14:58:05 +0800

    Updated scripts.

--- home/centos/scripts/deployment-restart.tomcat.sh
+++ home/centos/scripts/deployment-restart.tomcat.sh
@@ -2,6 +2,22 @@
 sudo systemctl stop tomcat
 sudo mkdir $TOMCAT_WEBAPPS/pmsys
 sudo systemctl start tomcat
-sleep 45s 
+
+# While the tomcat is starting, sleep for 60 seconds.
+echo "Sleeping for 60 seconds. Waiting for tomcat."
+sleep 60s 
+
+# If curl is empty, tomcat is ready.
+tomcatCurl=$(curl localhost:8080/manager)
+tomcatCurlResult=$(echo ${tomcatCurl})
+
+# If curl is not empty, sleep again.
+if [-n "$tomcatCurlResult"]
+then
+	echo "Tomcat not yet ready. Sleeping again."
+	sleep 60s
+	
+# By now, tomcat should be ready.
+echo "Removing /pmsys directory."
 sudo rm -dR $TOMCAT_WEBAPPS/pmsys
 echo "Restarted tomcat."
