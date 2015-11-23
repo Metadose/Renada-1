b7f4aa87e7d20861aa94ffaf57fbb080b70c26d6
VicCebedo <cebedo.vii@gmail.com>
2015-11-23 15:26:51 +0800

    Added more scripts.

---
+++ home/centos/scripts/deployment-finish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start services.
+sh ~/scripts/deployment-start.services.sh
+
+# Start the tomcat.
+sudo systemctl restart tomcat
+
+# Sleep for 60 seconds to wait.
+sleep 60s
+
+echo "Deployment is finished."
