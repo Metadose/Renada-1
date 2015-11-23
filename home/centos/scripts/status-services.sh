9ccdc0458875aa74a4717af2118cc7b77effd0f6
VicCebedo <cebedo.vii@gmail.com>
2015-11-23 14:13:04 +0800

    Updated deployment script.

---
+++ home/centos/scripts/status-services.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sudo systemctl status nginx -l
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sudo systemctl status activemq -l
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sudo systemctl status mysqld -l
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sudo systemctl status redis -l
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sudo systemctl status tomcat -l
