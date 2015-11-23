3ab29e52b2e5c563d9f0e6cce8a67e8918a2ae01
VicCebedo <cebedo.vii@gmail.com>
2015-11-23 15:43:46 +0800

    Updated deployment script.

---
+++ home/centos/scripts/tomcat-force-undeploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Force tomcat undeploy and clean.
+
+sudo systemctl stop tomcat
+cd $TOMCAT_WEBAPPS
+sudo rm -Rd pmsys
+sudo rm pmsys.war
+sh ~/scripts/deployment-restart.tomcat.sh
+echo "Forced undeploy finished."
