a1af24d69f28b550b2b585d3c5f5b68b4ec861d2
VicCebedo <cebedo.vii@gmail.com>
2015-11-27 14:51:01 +0800

    Disabled ActiveMQ in the deployment scripts.

--- home/centos/scripts/deployment/stop-services.sh
+++ home/centos/scripts/deployment/stop-services.sh
@@ -2,8 +2,8 @@
 sudo systemctl stop nginx
 echo "Stopped nginx."
 
-sudo systemctl stop activemq
-echo "Stopped activemq."
+# sudo systemctl stop activemq
+# echo "Stopped activemq."
 
 sudo systemctl stop redis
 echo "Stopped redis."
