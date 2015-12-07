5e9d95b5acfb9c15589f3d51d4b021a95661ffd5
VicCebedo <cebedo.vii@gmail.com>
2015-12-07 17:11:32 +0800

    Updated update script.

--- home/centos/scripts/git/update-renada.sh
+++ home/centos/scripts/git/update-renada.sh
@@ -2,7 +2,7 @@
 
 # Update the Renada repository and copy the scripts.
 cd $RENADA_HOME
-git pull
+sudo git pull
 sudo rm -Rd /home/centos/scripts/
 sudo mkdir -p /home/centos/scripts
 sudo cp -r home/centos/scripts/* /home/centos/scripts
