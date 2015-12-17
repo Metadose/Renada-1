2193e029f708c17c587164658592c49a9a3f4939
VicCebedo <cebedo.vii@gmail.com>
2015-12-17 09:09:42 +0800

    Updated clear nginx log script.

--- home/centos/scripts/clear-nginx-logs.sh
+++ home/centos/scripts/clear-nginx-logs.sh
@@ -8,4 +8,7 @@ sudo rm $NGINX_LOGS/error.log
 sudo touch $NGINX_LOGS/access.log
 sudo touch $NGINX_LOGS/error.log
 
+# Change permission to be writable by all.
+sudo chmod 666 -R $NGINX_LOGS/*.log
+ 
 echo "Nginx logs cleared"
